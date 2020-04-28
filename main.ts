b7f302ef80e4ced78b24e0b7504d0a49c7dc4ccd
PinsentL <64485558+PinsentL@users.noreply.github.com>
2020-04-28 18:50:23 +0100

    Came up with my game idea and started to lay out the basics.

--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
-
+info.setScore(0)
+info.setLife(3)
+scene.setBackgroundColor(2)
+let mySprite = sprites.create(img`
+. . 6 6 6 6 . . 
+. 6 1 4 4 4 6 . 
+6 d 4 4 4 4 4 6 
+c b b 1 1 4 d c 
+. c b b 4 1 c . 
+. . c c c c . . 
+`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setPosition(0, 60)
+let mySprite3 = sprites.create(img`
+f f f f f f f f f f f f f f f f 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+f f f f f f f f f f f f f f f f 
+`, SpriteKind.Player)
