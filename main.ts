835725b81a1f8e7b0610eb600a3631907a974fa7
PinsentL <64485558+PinsentL@users.noreply.github.com>
2020-04-29 16:34:24 +0100

    completed level 2 and 3.

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+namespace SpriteKind {
+    export const Food1 = SpriteKind.create()
+    export const Food2 = SpriteKind.create()
+}
 namespace myTiles {
     //% blockIdentity=images._tile
     export const tile0 = img`
@@ -20,11 +24,11 @@ namespace myTiles {
 `
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.builtin.brick, function (sprite, location) {
-    mySprite.setPosition(20, 0)
+    mySprite.setPosition(20, 20)
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.builtin.oceanDepths0, function (sprite, location) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     tiles.setTilemap(tiles.createTilemap(
-            hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010000000101010101000000000000000100000001000000000000000000040001000000010000000000000000000000010000000100000000000000000000000100000001000001010101010000000001000000010000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000101010000010101010101000000000000000000000100000000000000000000000000000001000000000000000000000000000000010003000000000000`,
+            hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010101010101010101000000000000000000000000000000010000000000000000000000000000000100000000000101010101010101010101000000000001000000000000000000000000000000010000000000000000000000000000000101010101010101010101000000000000000000000000000000000000000000000000000000000000000000000000000101010101010101010101000000000001000000000000000000000000000000010000000000000000000000000000000100000000000000000000000000`,
             img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -43,15 +47,35 @@ scene.onOverlapTile(SpriteKind.Player, sprites.builtin.oceanDepths0, function (s
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `,
-            [myTiles.tile0,sprites.builtin.brick,sprites.builtin.forestTiles26,sprites.builtin.forestTiles25,sprites.builtin.coral1],
+            [myTiles.tile0,sprites.builtin.brick],
             TileScale.Sixteen
         ))
     mySprite.setPosition(20, 20)
-})
-scene.onOverlapTile(SpriteKind.Player, sprites.builtin.coral0, function (sprite, location) {
+    mySprite3 = sprites.create(img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
+`, SpriteKind.Food1)
+    mySprite3.setPosition(120, 75)
     info.changeScoreBy(1)
+    mySprite2.destroy()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food1, function (sprite, otherSprite) {
     tiles.setTilemap(tiles.createTilemap(
-            hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010101010101010100000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010101000001010101000000000000000000010000000000000000000000000000000100000000000000000000000000000000000000010000000000000003000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000`,
+            hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010101010101010000010101000000000000000000000100000000000000000000000000000001000000000000000000000001000000010000000000000000000000010000000100000000000101010100000100000000000001010100000000000001000000000000000000000000000000010000000000000000000000000000000101010101010101010100000101000000000000000100000000000000010000000000000000000000000000000100000000000000000000000000000001000000000100000000000000`,
             img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -70,39 +94,36 @@ scene.onOverlapTile(SpriteKind.Player, sprites.builtin.coral0, function (sprite,
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `,
-            [myTiles.tile0,sprites.builtin.brick,sprites.castle.shrub,sprites.builtin.oceanDepths0],
+            [myTiles.tile0,sprites.builtin.brick],
             TileScale.Sixteen
         ))
+    mySprite.setPosition(20, 20)
+    mySprite4 = sprites.create(img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
+`, SpriteKind.Food2)
+    mySprite4.setPosition(170, 225)
+    mySprite3.destroy()
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.builtin.coral1, function (sprite, location) {
-    info.changeScoreBy(1)
-    tiles.setTilemap(tiles.createTilemap(
-            hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010000000001010101010000000000000100000000010000000000000000000001000000000100000000000000000000010000000001000000000000000000000100000000010000010101010000000001000000000100000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000001010100000101010101000000000000000000000001000000000000000000000000000000010000000000000000000000000000000100020000000000`,
-            img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`,
-            [myTiles.tile0,sprites.builtin.brick,sprites.builtin.oceanDepths1],
-            TileScale.Sixteen
-        ))
-})
+let mySprite4: Sprite = null
+let mySprite3: Sprite = null
+let mySprite2: Sprite = null
 let mySprite: Sprite = null
 scene.setBackgroundColor(7)
-scene.centerCameraAt(0, 0)
 info.setScore(0)
 mySprite = sprites.create(img`
 . . . . . . . . . . . . . . . . . . . . . . . . 
@@ -132,8 +153,9 @@ f b b b b f 2 2 2 2 f d 4 . . . . . . . . . . .
 `, SpriteKind.Player)
 mySprite.setPosition(20, 20)
 controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
 tiles.setTilemap(tiles.createTilemap(
-            hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010101010101010100000000000000000000000000000001000000000000000000000000000000000000000000000700000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010101000001010101000000000000000000010000000000000000000000000000000100000000000000000000000000000000000000010000000000000006000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000`,
+            hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010101010101010100000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010101000001010101000000000000000000010000000000000000000000000000000100000000000000000000000000000000000000010000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000`,
             img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -155,5 +177,23 @@ tiles.setTilemap(tiles.createTilemap(
             [myTiles.tile0,sprites.builtin.brick,sprites.castle.shrub,sprites.builtin.crowd8,sprites.dungeon.buttonPink,sprites.builtin.coral5,sprites.builtin.oceanDepths0,sprites.builtin.coral0],
             TileScale.Sixteen
         ))
-scene.cameraFollowSprite(mySprite)
 info.setLife(3)
+mySprite2 = sprites.create(img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
+`, SpriteKind.Food)
+mySprite2.setPosition(10, 200)
