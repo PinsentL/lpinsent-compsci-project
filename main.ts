e6aa40ae905260ac543482268496f5397a1df2e1
PinsentL <64485558+PinsentL@users.noreply.github.com>
2020-04-30 08:20:02 +0100

    Completed coding level 4 and introduced lava.

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 namespace SpriteKind {
     export const Food1 = SpriteKind.create()
     export const Food2 = SpriteKind.create()
+    export const Food3 = SpriteKind.create()
+    export const Food4 = SpriteKind.create()
 }
 namespace myTiles {
     //% blockIdentity=images._tile
@@ -23,6 +25,53 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food3, function (sprite, otherSprite) {
+    tiles.setTilemap(tiles.createTilemap(
+            hex`1000100000000000000000000000000000000000000000000000000000000000000000000101010101010000010101000001010100000000000100000100010000000000000000000002000001000200000000000000010102020000010000000002010100000000000000000100000000010000000001000000000001000000000100000000010102020000010000000001000000000000000000000100000000010000000000000000000001000002020100000000010101010000020000010000000000000000000100000200000100000000000001010202000001000001020200000000000000000000010000000000000000000000000000000100000000000000`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,sprites.builtin.brick,sprites.dungeon.hazardLava1],
+            TileScale.Sixteen
+        ))
+    mySprite.setPosition(20, 20)
+    mySprite6 = sprites.create(img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
+`, SpriteKind.Food4)
+    mySprite6.setPosition(200, 200)
+    info.changeScoreBy(1)
+    mySprite5.destroy()
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.builtin.brick, function (sprite, location) {
     mySprite.setPosition(20, 20)
 })
@@ -73,6 +122,10 @@ e e b e c c e e e e e c e b e e
     info.changeScoreBy(1)
     mySprite2.destroy()
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
+    info.changeLifeBy(-1)
+    mySprite.setPosition(20, 20)
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food1, function (sprite, otherSprite) {
     tiles.setTilemap(tiles.createTilemap(
             hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010101010101010101010000010101000000000000000000000100000000000000000000000000000001000000000000000000000001000000010000000000000000000000010000000100000000000101010100000100000000000001010100000000000001000000000000000000000000000000010000000000000000000000000000000101010101010101010100000101000000000000000100000000000000010000000000000000000000000000000100000000000000000000000000000001000000000100000000000000`,
@@ -117,10 +170,60 @@ e e b e c c e e e e e c e b e e
 . . . c c c c c e e e e e . . . 
 `, SpriteKind.Food2)
     mySprite4.setPosition(170, 225)
+    info.changeScoreBy(1)
     mySprite3.destroy()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food2, function (sprite, otherSprite) {
+    tiles.setTilemap(tiles.createTilemap(
+            hex`1000100000000000000000000000000000000000000000000000000000000000000000000101010101000001010101010101000000000000010000000000010000010000000000000100000000000000000101010000000001000001000000000001000001010000010101010101000000010000000000000000010000010000000100000000000000000100000100000001000000000101000001000001000001010000000001000000000000010000000100000000010000000000000100000001000000000101010101010101000000010000000000000100000000010000000000000000000000000000000000000000000000000000000000000000000000000000`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,sprites.builtin.brick],
+            TileScale.Sixteen
+        ))
+    mySprite.setPosition(20, 20)
+    mySprite5 = sprites.create(img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
+`, SpriteKind.Food3)
+    mySprite5.setPosition(20, 60)
+    info.changeScoreBy(1)
+    mySprite4.destroy()
 })
 let mySprite4: Sprite = null
 let mySprite3: Sprite = null
+let mySprite5: Sprite = null
+let mySprite6: Sprite = null
 let mySprite2: Sprite = null
 let mySprite: Sprite = null
 scene.setBackgroundColor(7)
