74b45dd54e06750f919730bbd25b57fdfd13c4d0
PinsentL <64485558+PinsentL@users.noreply.github.com>
2020-05-01 08:46:37 +0100

    Added another level

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ namespace SpriteKind {
     export const Food4 = SpriteKind.create()
     export const Food5 = SpriteKind.create()
     export const Food6 = SpriteKind.create()
+    export const Food7 = SpriteKind.create()
 }
 namespace myTiles {
     //% blockIdentity=images._tile
@@ -94,7 +95,7 @@ e e b e c c e e e e e c e b e e
     mySprite5.destroy()
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.builtin.brick, function (sprite, location) {
-    mySprite.setPosition(20, 20)
+	
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     tiles.setTilemap(tiles.createTilemap(
@@ -143,9 +144,56 @@ e e b e c c e e e e e c e b e e
     info.changeScoreBy(1)
     mySprite2.destroy()
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
-    info.changeLifeBy(-1)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food6, function (sprite, otherSprite) {
+    info.startCountdown(15)
+    tiles.setTilemap(tiles.createTilemap(
+            hex`1000100000000000000000000000000000000000000000000000000000000000000000000203000003020202030000030202020200000000000000000200000200000000000000000000000002000002000000000000030303030303030000030300000000000200000000000000000002000000000002000000000000000000020000000000020202020303030000000200000000000200000000000000000002000000000002000000000000000000020000000000030000030202030000000300000000000300000200000200000003000000000003020202000003030000030000000000000000000000000000000000000000000000000000000000000000000000`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1,sprites.builtin.brick,sprites.dungeon.hazardLava1],
+            TileScale.Sixteen
+        ))
     mySprite.setPosition(20, 20)
+    mySprite9 = sprites.create(img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
+`, SpriteKind.Food7)
+    mySprite9.setPosition(220, 220)
+    info.changeScoreBy(1)
+    mySprite8.destroy()
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
+	
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food1, function (sprite, otherSprite) {
     tiles.setTilemap(tiles.createTilemap(
@@ -241,6 +289,11 @@ e e b e c c e e e e e c e b e e
     mySprite8.setPosition(20, 60)
     info.changeScoreBy(1)
     mySprite7.destroy()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food7, function (sprite, otherSprite) {
+    effects.confetti.startScreenEffect()
+    pause(100)
+    effects.confetti.endScreenEffect()
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food2, function (sprite, otherSprite) {
     tiles.setTilemap(tiles.createTilemap(
@@ -338,8 +391,9 @@ e e b e c c e e e e e c e b e e
     mySprite6.destroy()
 })
 let mySprite7: Sprite = null
-let mySprite8: Sprite = null
 let mySprite4: Sprite = null
+let mySprite8: Sprite = null
+let mySprite9: Sprite = null
 let mySprite3: Sprite = null
 let mySprite5: Sprite = null
 let mySprite6: Sprite = null
