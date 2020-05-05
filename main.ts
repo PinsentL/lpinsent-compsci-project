6b83fc01c32fd46fecad6e3d5d66ba02a2fcf532
PinsentL <64485558+PinsentL@users.noreply.github.com>
2020-05-05 10:15:16 +0100

    added another level and finished the game

--- main.ts
+++ main.ts
@@ -6,6 +6,7 @@ namespace SpriteKind {
     export const Food5 = SpriteKind.create()
     export const Food6 = SpriteKind.create()
     export const Food7 = SpriteKind.create()
+    export const Food8 = SpriteKind.create()
 }
 namespace myTiles {
     //% blockIdentity=images._tile
@@ -94,8 +95,13 @@ e e b e c c e e e e e c e b e e
     info.changeScoreBy(1)
     mySprite5.destroy()
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food8, function (sprite, otherSprite) {
+    effects.confetti.startScreenEffect()
+    pause(5000)
+    effects.confetti.endScreenEffect()
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.builtin.brick, function (sprite, location) {
-	
+    mySprite.setPosition(20, 20)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     tiles.setTilemap(tiles.createTilemap(
@@ -193,7 +199,8 @@ e e b e c c e e e e e c e b e e
     mySprite8.destroy()
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
-	
+    info.changeLifeBy(-1)
+    mySprite.setPosition(20, 20)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food1, function (sprite, otherSprite) {
     tiles.setTilemap(tiles.createTilemap(
@@ -291,9 +298,52 @@ e e b e c c e e e e e c e b e e
     mySprite7.destroy()
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food7, function (sprite, otherSprite) {
-    effects.confetti.startScreenEffect()
-    pause(100)
-    effects.confetti.endScreenEffect()
+    info.startCountdown(10)
+    tiles.setTilemap(tiles.createTilemap(
+            hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000302020202020202020203000000000003000000000000000000030000000000020000000000000000000200000000000200000000000000000002000000000002000000000000000000020000000000020000000000000000000200000000000200000000000000000002000000000002000000000000000000020000000000020000000000000000000200000000000200000000000000000002000000000003030202030000030203030000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`,
+            img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1,sprites.builtin.brick,sprites.dungeon.hazardLava1],
+            TileScale.Sixteen
+        ))
+    mySprite.setPosition(20, 20)
+    mySprite10 = sprites.create(img`
+. . . . c c c b b b b b . . . . 
+. . c c b 4 4 4 4 4 4 b b b . . 
+. c c 4 4 4 4 4 5 4 4 4 4 b c . 
+. e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+. e b 4 4 4 4 4 5 4 4 4 4 b e . 
+8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+8 7 2 e e e e e e e e e e 2 7 8 
+e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+e b e 8 8 c c 8 8 c c c 8 e b e 
+e e b e c c e e e e e c e b e e 
+. e e b b 4 4 4 4 4 4 4 4 e e . 
+. . . c c c c c e e e e e . . . 
+`, SpriteKind.Food8)
+    mySprite10.setPosition(100, 100)
+    info.changeScoreBy(1)
+    mySprite9.destroy()
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food2, function (sprite, otherSprite) {
     tiles.setTilemap(tiles.createTilemap(
@@ -390,6 +440,7 @@ e e b e c c e e e e e c e b e e
     info.changeScoreBy(1)
     mySprite6.destroy()
 })
+let mySprite10: Sprite = null
 let mySprite7: Sprite = null
 let mySprite4: Sprite = null
 let mySprite8: Sprite = null
